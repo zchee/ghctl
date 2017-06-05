e3e834ec091b512c1917030f74f12abac066492c
Koichi Shiraishi <zchee.io@gmail.com>
2017-06-05 18:32:13 +0900

    request: add github package request wrapper function
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ request.go
@@ -0,0 +1,19 @@
+// Copyright 2017 The ghctl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+)
+
+func getUser(ctx context.Context, client *github.Client) (*github.User, error) {
+	user, _, err := client.Users.Get(ctx, "")
+	if _, ok := err.(*github.RateLimitError); ok {
+		return nil, ErrRateLimit
+	}
+	return user, nil
+}
