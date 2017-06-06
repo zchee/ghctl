f8c472ab5d99debf0330c52e1361a6ea159bf764
Koichi Shiraishi <zchee.io@gmail.com>
2017-06-07 01:12:10 +0900

    internal/errors: add caller function and simplitify
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -19,70 +19,56 @@ func New(msg string) error {
 }
 
 func Errorf(format string, a ...interface{}) error {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		locerr.Errorf(format, a...)
+	pos := caller()
+	if pos == nil {
+		return locerr.Errorf(format, a...)
 	}
-	pos := locerr.Pos{
-		File: &locerr.Source{
-			Path:   file,
-			Exists: true,
-		},
-		Line: line,
-	}
-	return locerr.ErrorfAt(pos, format, a...)
+	return locerr.ErrorfAt(*pos, format, a...)
 }
 
 func Wrap(err error, msg string) error {
 	if err == nil {
 		return nil
 	}
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		locerr.Note(err, msg)
+	pos := caller()
+	if pos == nil {
+		return locerr.Note(err, msg)
 	}
-	pos := locerr.Pos{
-		File: &locerr.Source{
-			Path:   file,
-			Exists: true,
-		},
-		Line: line,
-	}
-	return locerr.NoteAt(pos, err, msg)
+	return locerr.NoteAt(*pos, err, msg)
 }
 
 func Wrapf(err error, format string, a ...interface{}) error {
 	if err == nil {
 		return nil
 	}
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		locerr.Notef(err, format, a...)
+	pos := caller()
+	if pos == nil {
+		return locerr.Notef(err, format, a...)
 	}
-	pos := locerr.Pos{
-		File: &locerr.Source{
-			Path:   file,
-			Exists: true,
-		},
-		Line: line,
-	}
-	return locerr.NotefAt(pos, err, format, a...)
+	return locerr.NotefAt(*pos, err, format, a...)
 }
 
 func WithStack(err error) error {
 	if err == nil {
 		return nil
 	}
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		locerr.NewError(err.Error())
+	pos := caller()
+	if pos == nil {
+		return locerr.NewError(err.Error())
 	}
-	pos := locerr.Pos{
+	return locerr.WithPos(*pos, err)
+}
+
+func caller() *locerr.Pos {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return nil
+	}
+	return &locerr.Pos{
 		File: &locerr.Source{
 			Path:   file,
 			Exists: true,
 		},
 		Line: line,
 	}
-	return locerr.WithPos(pos, err)
 }
