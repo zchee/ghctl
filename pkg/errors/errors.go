2997f51372f925eaace47ae1423cd7a36b769ce3
Koichi Shiraishi <zchee.io@gmail.com>
2018-04-17 08:42:36 +0900

    pkg/errors: add errors package

---
+++ pkg/errors/errors.go
@@ -0,0 +1,22 @@
+// Copyright 2018 The ghctl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import (
+	"errors"
+
+	"github.com/google/go-github/github"
+)
+
+var (
+	ErrRateLimit = errors.New("hit GitHub API rate limit")
+)
+
+func IsRateLimitErr(err error) bool {
+	if _, ok := err.(*github.RateLimitError); ok {
+		return true
+	}
+	return false
+}
